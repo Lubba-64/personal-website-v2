ba4fc3712ed1a9ae6a76e24af1344dd9d8503709
Benjamin Hines <75751722+Lubba-64@users.noreply.github.com>
2022-08-18 19:14:24 -0400

    chore: Simplify contact page

--- src/App/Pages/Contact.tsx
+++ src/App/Pages/Contact.tsx
@@ -10,12 +10,7 @@ export const Contact = () => {
         <p>
           Contacting me is as easy as sending me an email, which is my perferred
           method <b>(Lubba64@gmail.com)</b>, or messaging me on discord{" "}
-          <b>(Lubba64#5426)</b>. I do have a phone number, but it is private. If
-          you need to do a voice call with me discord is the only option (unless
-          you've sent me an email and I deem it fit to give you my phone number)
-          I am totally open to your messages about job opportunities, or other
-          buisiness related inquiries If its for some other reason, thats ok
-          too.
+          <b>(Lubba64#5426)</b>.
         </p>
       </ContentPanel>
     </div>
